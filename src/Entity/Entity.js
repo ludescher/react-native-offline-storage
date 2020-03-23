55557bc9a91f478ec3735c4e3acbd5519cfa068e
Johannes Ludescher <ludescher.j@gmail.com>
2020-03-23 16:18:08 +0100

    no message

--- src/Entity/Entity.js
+++ src/Entity/Entity.js
@@ -4,7 +4,6 @@ export default class Entity {
     static Iri = null; //Internationalized Resource Identifier (replaces class name)
     static TypeMap = {};
     static UniqueProperty = "id";
-    static SortBy = "id";
 
     entityId = null;
 
