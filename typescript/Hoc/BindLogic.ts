3997cff0dc276e1b34b032ba4dc83bc17709b8b3
Johannes Ludescher <ludescher.j@gmail.com>
2020-05-22 18:40:21 +0200

    started to implement a mvc-hoc

---
+++ typescript/Hoc/BindLogic.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
+
+function BindLogic(test: any) {
+
+    console.log("MVC()", test);
+
+}
+
+export default BindLogic;
