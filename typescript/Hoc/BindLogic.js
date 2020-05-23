aa212f37ba0ae02ce6bb764d0709c564c1ffe4d0
Johannes Ludescher <ludescher.j@gmail.com>
2020-05-23 14:12:12 +0200

    test run #1

--- typescript/Hoc/BindLogic.js
+++ typescript/Hoc/BindLogic.js
@@ -1,43 +1,50 @@
 import React from 'react';
 import ComponentError from '../Error/ComponentError';
+import AbstractComponentLogic from '../React/AbstractComponentLogic';
 
-function BindLogic(WrappedComponent, Controller) {
+function BindLogic(Controller) {
 
-    console.log("BindLogic()", { WrappedComponent, Controller });
+    console.log("BindLogic()", Controller);
 
-    if (!(WrappedComponent instanceof React.Component)) {
-        throw new ComponentError('WrappedComponent has to be of type React.Component');
+    if (!(Logic instanceof AbstractComponentLogic)) {
+        throw new ComponentError('Logic has to be of type AbstractComponentLogic');
     }
 
-    class LogicComponent extends React.Component {
-
-        /**
-         * @type {Object}
-         */
-        state = {};
-
-        constructor(props) {
-            super(props);
+    return (WrappedComponent) => {
+        if (!(WrappedComponent instanceof React.Component)) {
+            throw new ComponentError('WrappedComponent has to be of type React.Component');
         }
 
-        render() {
-            const { forwardedRef, ...props } = this.props;
-            return <WrappedComponent ref={forwardedRef} {...props} {...this.state} />
+        class LogicComponent extends React.Component {
+
+            /**
+             * @type {Object}
+             */
+            state = {};
+
+            constructor(props) {
+                super(props);
+            }
+
+            render() {
+                const { forwardedRef, ...props } = this.props;
+                return <WrappedComponent ref={forwardedRef} {...props} {...this.state} />
+            }
         }
+
+        const _forwarded = React.forwardRef((props, ref) => {
+            return <LogicComponent {...props} forwardedRef={ref} />
+        });
+
+        _forwarded.displayName = `LogicComponent(${getDisplayName(WrappedComponent)})`;
+        _forwarded.WrappedComponent = WrappedComponent;
+
+        return hoistStatics(_forwarded, WrappedComponent);
     }
 
-    const _forwarded = React.forwardRef((props, ref) => {
-        return <LogicComponent {...props} forwardedRef={ref} />
-    });
-
-    _forwarded.displayName = `LogicComponent(${getDisplayName(WrappedComponent)})`;
-    _forwarded.WrappedComponent = WrappedComponent;
-
-    return hoistStatics(_forwarded, WrappedComponent);
-}
-
-function getDisplayName(WrappedComponent) {
-    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+    function getDisplayName(WrappedComponent) {
+        return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+    }
 }
 
 export default BindLogic;
